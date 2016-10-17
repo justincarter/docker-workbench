dc4370ba0596a388425402d6102f91f7e35ae6fb
Justin Carter <justin.w.carter@gmail.com>
2016-10-16 16:39:14 -1000

    add run package tests

--- run/run_test.go
+++ run/run_test.go
@@ -1,6 +1,8 @@
 package run
 
 import (
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -17,4 +19,26 @@ func TestOutput(t *testing.T) {
 		t.Fail()
 	}
 
+}
+
+func TestVBoxManagePath_Path(t *testing.T) {
+	testVBoxManagePath(t)
+}
+
+func TestVBoxManagePath_InstallPath(t *testing.T) {
+	os.Setenv("VBOX_INSTALL_PATH", "/dummy/install/path")
+	testVBoxManagePath(t)
+}
+
+func TestVBoxManagePath_MSIInstallPath(t *testing.T) {
+	os.Setenv("VBOX_MSI_INSTALL_PATH", "/dummy/msi/install/path")
+	testVBoxManagePath(t)
+}
+
+func testVBoxManagePath(t *testing.T) {
+	path := VBoxManagePath()
+	s := strings.Split(path, string(os.PathSeparator))
+	if s[len(s)-1] != "VBoxManage" {
+		t.Fail()
+	}
 }
